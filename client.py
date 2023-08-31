f3f4477e9128d9c79f480156cad06af95cc0ae67
Sai Arun Vakacherla <92098685+Saiarun1856@users.noreply.github.com>
2023-08-31 23:26:42 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,11 @@
 import asyncio
 from websockets.sync.client import connect
 
-def hello():
-    with connect("ws://localhost:8765") as websocket:
+def servers():
+    with connect("ws://localhost:6000") as websocket:
         websocket.send("Hello message from server !")
         message = websocket.recv()
         print(f"Received: {message}")
         msg2 = websocket.recv()
-        
 
-hello()
+servers()
